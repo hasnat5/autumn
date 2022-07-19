5a96e869e89ce0e81b7124cbd60e10091542600e
Hasnat <93985099+hasnat5@users.noreply.github.com>
2022-07-19 20:31:53 +0700

    add file

---
+++ script.js
@@ -0,0 +1,12 @@
+const tombol2 = document.querySelector('.btnBox p');
+
+
+tombol2.addEventListener('click', function(){
+  const tolak = confirm("are you sure?");
+  if(tolak === true){
+    location.href = "https://www.google.com/";
+  }
+});
+
+
+
