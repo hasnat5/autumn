7421b3bd8ec413cec22c847d1a6bf76307c91385
hasnat5 <hasnatferdiananda5@gmail.com>
2022-07-20 15:06:50 +0700

    fix styles css

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const tombol2 = document.querySelector('.btnBox p');
 
 
 tombol2.addEventListener('click', function(){
-  const tolak = confirm("are you sure?");
-  if(tolak === true){
-    location.href = "https://www.google.com/";
-  }
+    const tolak = confirm("are you sure?");
+    if(tolak === true){
+        location.href = "https://www.google.com/";
+    }
 });
 
 
